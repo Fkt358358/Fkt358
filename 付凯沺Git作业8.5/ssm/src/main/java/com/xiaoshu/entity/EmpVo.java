268adbb5231e2e23cb1b58c7fe1bd682fa7d87b0
Fkt358358 <1472747398@qq.com>
2020-08-05 22:36:59 +0800

    8.5作业提交

---
+++ 付凯沺Git作业8.5/ssm/src/main/java/com/xiaoshu/entity/EmpVo.java
@@ -0,0 +1,34 @@
+package com.xiaoshu.entity;
+
+public class EmpVo extends Emp {
+
+	private String dname;
+
+	private String end;
+	private String start;
+
+	public String getEnd() {
+		return end;
+	}
+
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+}
